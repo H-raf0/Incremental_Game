b01c19dfee187dedee6099569d2a18cc69a52edf
Achraf <achrafelallali123@gmail.com>
2026-01-31 14:17:15 +0200

    (fonc) adding online players counter

--- GameServerApi/Hubs/ChatHub.cs
+++ GameServerApi/Hubs/ChatHub.cs
@@ -5,6 +5,22 @@ namespace GameServerApi;
 
 public class ChatHub : Hub
 {
+    private static int _onlinePlayerCount = 0;
+
+    public override async Task OnConnectedAsync()
+    {
+        _onlinePlayerCount++;
+        await Clients.All.SendAsync("UpdateUserCount", _onlinePlayerCount);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        _onlinePlayerCount--;
+        await Clients.All.SendAsync("UpdateUserCount", _onlinePlayerCount);
+        await base.OnDisconnectedAsync(exception);
+    }
+
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
